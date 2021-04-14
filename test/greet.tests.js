ebc629d7cb49f9d680b92ac1a6d22b83e9e2ba82
tshifhiwa <matombot@gmail.com>
2021-04-14 12:49:36 +0200

    initial commit

---
+++ test/greet.tests.js
@@ -0,0 +1,6 @@
+describe('this test should greet' , function(){
+    it('should return "Hello tshifhiwa"' , function(){
+    assert.equal("Hello, tshifhiwa",greet("tshifhiwa"))
+
+    });
+});
---
+++ test/isFromBellville.tests.js
@@ -0,0 +1,6 @@
+describe('this test should check if regno isFromBellville' , function(){
+    it('should return true if it is CY else false' , function(){
+    assert.equal(false,isFromBellville("Ci"))
+
+    });
+});
