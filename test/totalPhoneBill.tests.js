caf526369e4df3860d779ea2afe9bdc6c74a3886
tshifhiwa <matombot@gmail.com>
2021-04-15 21:18:53 +0200

    tests all

--- test/totalPhoneBill.tests.js
+++ test/totalPhoneBill.tests.js
@@ -1,4 +1,4 @@
-describe('this test should show the total bill for the data provided call, sms, call, sms, sms', function(){
+describe('this test should show the total bill for the data provided', function(){
     it('returns the total price for data R7.45,totalPhoneBill("call, sms, call, sms, sms")', function(){
     assert.equal("R7.45",totalPhoneBill("call, sms, call, sms, sms"))
     assert.equal("R3.40",totalPhoneBill("call,sms"))
