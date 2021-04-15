21c7b39808b90a66a35f86086f4dfe85698f3624
tshifhiwa <matombot@gmail.com>
2021-04-15 21:11:17 +0200

    commit tests

--- test/totalPhoneBill.tests.js
+++ test/totalPhoneBill.tests.js
@@ -1,5 +1,5 @@
 describe('this test should show the total bill for the data provided call, sms, call, sms, sms', function(){
-    it('returns the total price for data R7.45,totalPhoneBill("")', function(){
+    it('returns the total price for data R7.45,totalPhoneBill("call, sms, call, sms, sms")', function(){
     assert.equal("R7.45",totalPhoneBill("call, sms, call, sms, sms"))
     assert.equal("R3.40",totalPhoneBill("call,sms"))
     assert.equal("R0.65",totalPhoneBill("sms"))
