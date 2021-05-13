fd41e0687c9cf764a9087aecee257b42812d8d74
tshifhiwa <matombot@gmail.com>
2021-05-13 13:41:31 +0200

    modified tests

--- test/findItemsOver.tests.js
+++ test/findItemsOver.tests.js
@@ -1,14 +1,11 @@
-describe('this test finditemsover and provide the items', function(){
-    it('returns item[ {name : "pears", qty : 37},{name : "bananas", qty : 27},{"name":"apples","qty":40}],("20")', function(){
-        // assert.equal([{"name":"pears","qty":"37"},{"name":"bananas","qty":"27"}],findItemsOver([{"name":"apples","qty":10},{"name":"pears","qty":37},{"name":"bananas","qty":27},{"name":"apples","qty":3}]))
-         });
-
-     it('returns item[ {name : "pears", qty : 37},{name : "bananas", qty : 27},{"name":"apples","qty":40}],("25")', function(){
-   // assert.equal([{"name":"pears","qty":"37"},{"name":"bananas","qty":"27"}],findItemsOver([{"name":"apples","qty":10},{"name":"pears","qty":37},{"name":"bananas","qty":27},{"name":"apples","qty":3}]))
+describe('this test finditemsover and provide the items', function () {
+    it('returns item[ {name : "pears", qty : 37},{name : "bananas", qty : 27},{"name":"apples","qty":40}],("20")', function () {
+        assert.deepEqual([], findItemsOver([{ "name": "bananas", "qty": 27 }, { "name": "apples", "qty": 3 }]))
     });
-    it('returns item[ {name : "pears", qty : 37},{"name":"apples","qty":40}],("30")', function(){
-        // assert.equal([{"name":"pears","qty":"37"},{"nam],findItemsOver([{"name":"apples","qty":10},{"name":"pears","qty":37},{"name":"bananas","qty":27},{"name":"apples","qty":3}]))
-         });
-     
-
+    it('returns item[ {name : "pears", qty : 37},{name : "bananas", qty : 27},{"name":"apples","qty":40}],("25")', function () {
+        assert.deepEqual([], findItemsOver([{ "name": "apples", "qty": 10 }, { "name": "pears", "qty": 37 }, { "name": "bananas", "qty": 27 }, { "name": "apples", "qty": 3 }]))
+    });
+    it('returns item[ {name : "pears", qty : 37},{"name":"apples","qty":40}],("30")', function () {
+        assert.deepEqual([], findItemsOver([{ "name": "apples", "qty": 10 }, { "name": "pears", "qty": 37 }, { "name": "bananas", "qty": 27 }, { "name": "apples", "qty": 3 }]))
+    });
 });
--- test/findItemsOver20.tests.js
+++ test/findItemsOver20.tests.js
@@ -1,9 +1,9 @@
 describe('this test find items over 20 and provide the items', function(){
     it('returns item[{name : "pears", qty : 37},{name : "bananas", qty : 27}]; ("20")', function(){
-   // assert.equal([{"name":"pears", "qty" : 37}],findItemsOver20([]))
+   assert.deepEqual([],findItemsOver20([]))
     });
-    it('returns item[{name : "pears", qty : 37},{name : "bananas", qty : 27}]; ("25")', function(){
-        // assert.equal([{"name":"pears", "qty" : 37}],findItemsOver20([]))
-         });
+         it('returns item[ {name : "pears", qty : 37},{name : "bananas", qty : 27},{"name":"apples","qty":40}],("20")', function () {
+            assert.deepEqual([], findItemsOver([{ "name": "bananas", "qty": 27 }, { "name": "apples", "qty": 3 }]))
+        });
 
 });
