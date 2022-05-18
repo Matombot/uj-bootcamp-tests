e2b1fabaa2a9c936e32a6ab05b8f307c036608cf
tshifhiwa <matombot@gmail.com>
2022-05-18 16:04:46 +0200

    initial commit

---
+++ 2022-folder/just/script.js
@@ -0,0 +1,34 @@
+const taxiRank = () => {
+    return {
+        taxiData: [
+            { destination: 'Makhaza', passagers: 12, departTaxi: 0, taxiFare: 12.00, taxiFareTotal: 0, availableTaxis:3},
+            { destination: 'Belhar', passagers: 34, departTaxi: 0, taxiFare: 15.00, taxiFareTotal: 0, availableTaxis:3 },
+            { destination: 'Bellville', passagers: 23, departTaxi: 0, taxiFare: 10.00, taxiFareTotal: 0, availableTaxis:3 }],
+        // passagers: 0,
+        // maxPassager:5,
+        // price: 0,
+        // departTaxi:0,
+        
+    //     total:0,
+    //     departedTaxis: function(){
+    //         if(this.passagers>=this.maxPassager){
+    //             this.availableTaxis-=1;
+    //             this.departTaxi+=1;
+    //             this.price = this.departTaxi*5*25
+    //             this.passagers=this.passagers-5; 
+    //         }   
+    //     },
+    //     addItem: function () {
+    //         this.passagers += 1;
+            
+    //     },
+    //     subItem: function(){
+    //         if(this.passagers<=0){
+    //             this.passagers=0
+    //         }else{
+    //             this.passagers-=1;
+    //         }   
+    //    }
+
+    }
+  }
